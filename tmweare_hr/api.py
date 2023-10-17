5cdad586fbf7d7b2e3a2cce09f73fca97a0c36c0
ujjwalkumar93 <pathakujjwal93@gmail.com>
2023-10-17 23:20:59 +0530

    fix: updated code for attendance

--- tmweare_hr/api.py
+++ tmweare_hr/api.py
@@ -1,25 +1,20 @@
 import frappe
 import math
 @frappe.whitelist()
-def calculate_distance(emp_lat, emp_long):
-    system_latitude = frappe.db.sql("select value from `tabSingles` where doctype = 'Attendance Setting' and field = 'latitude';", as_dict=1)
-    system_longitude = frappe.db.sql("select value from `tabSingles` where doctype = 'Attendance Setting' and field = 'longitude';", as_dict=1)
-    enable_location = int(frappe.db.sql("select value from `tabSingles` where doctype = 'Attendance Setting' and field = 'enable';", as_dict=1)[0].get('value'))
+def calculate_distance(emp_lat, emp_long, branch):
+    system_location_data = frappe.db.sql("select latitude,longitude, enable  from `tabBranch` where name = '{0}';".format(branch), as_dict=1)
     invalid_distance = 0
 
-    if enable_location == 1:
-        if not system_latitude or not system_longitude:
-            #frappe.throw('Ask admin to enter latitude & longitude in attendance setting')
+    if system_location_data and system_location_data[0].get('enable')  == 1:
+        if not system_location_data[0].latitude or not system_location_data[0].longitude:
             invalid_distance = 1
-            frappe.throw('Ask admin to enter latitude & longitude in attendance setting')
-            # return invalid_distance
-            
+            frappe.throw('Ask admin to enter latitude & longitude in <b> Branch </b> doctype')    
         else:
             # Radius of the Earth in kilometers
             radius = 6371.0
             # Convert latitude and longitude from degrees to radians
-            system_lat = math.radians(float(system_latitude[0].get('value')))
-            system_lon = math.radians(float(system_longitude[0].get('value')))
+            system_lat = math.radians(system_location_data[0].get('latitude'))
+            system_lon = math.radians(system_location_data[0].get('longitude'))
             emp_lat = math.radians(float(emp_lat))
             emp_lon = math.radians(float(emp_long))
 
@@ -29,9 +24,9 @@ def calculate_distance(emp_lat, emp_long):
             a = math.sin(dlat / 2)**2 + math.cos(system_lat) * math.cos(emp_lat) * math.sin(dlon / 2)**2
             c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
             employee_distance = radius * c * 1000
-            maximum_allowed_distance = float(frappe.db.sql("select value from `tabSingles` where doctype = 'Attendance Setting' and field = 'maximum_distance';", as_dict=1)[0].get('value'))
+            maximum_allowed_distance = frappe.db.sql("select custom_maximum_distance from `tabBranch` where name = '{0}';".format(branch), as_dict=1)
 
-            if employee_distance > maximum_allowed_distance:
+            if maximum_allowed_distance and employee_distance > maximum_allowed_distance[0].get('custom_maximum_distance'):
                 invalid_distance = 1
                 return invalid_distance
     return invalid_distance
