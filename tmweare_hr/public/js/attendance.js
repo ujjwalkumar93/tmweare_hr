ee1a0b7ea0f773a7eda16f951e6537ce9a1d7405
Ujjwalkumar93 <pathakujjwal93@gmail.com>
2023-10-17 13:52:47 +0530

    code added for attendance

---
+++ tmweare_hr/public/js/attendance.js
@@ -0,0 +1,40 @@
+frappe.ui.form.on('Attendance', {
+    before_save: function(frm){
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                // The position object contains latitude and longitude
+                const employeeLatitude = position.coords.latitude;
+                const employeeLongitude = position.coords.longitude;
+                frappe.call({
+                    method: 'tmweare_hr.api.calculate_distance',
+                    args : {
+                        emp_lat : employeeLatitude,
+                        emp_long : employeeLongitude
+                    },
+                    callback : (resp) => {
+                        console.log('responce is: ', resp.message)
+                    }
+                })
+            }, function (error) {
+                // Handle any errors that occur when trying to get the location
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        frappe.throw("User/System denied the request for geolocation.");
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        frappe.throw("Location information is unavailable.");
+                        break;
+                    case error.TIMEOUT:
+                        frappe.throw("The request to get user location timed out.");
+                        break;
+                    case error.UNKNOWN_ERROR:
+                        frappe.throw("An unknown error occurred.");
+                        break;
+                }
+            });
+        } else {
+            // Geolocation is not available in this browser
+            console.error("Geolocation is not available in this browser.");
+        }
+    }
+})
